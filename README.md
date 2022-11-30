eab61cf5f5fb7b838d19867590a9a9f40908a274
Rafael Dantas <rdaraujo@gmail.com>
2022-11-30 18:21:49 -0300

    Add modal error and README

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# react-max-udemy-time-to-practice
-react-max-udemy-time-to-practice
+# Guest List 
+
+This is a small project in React that implements a simple Guest Listing.
+
+It's the result of the Section 8 from the Max Schwarzmüller Udemy React Course.
+
+## How it's organized
+
+There's only one branch, called `main` and no tags.
+
+## How to use
+
+Just checkout the code, then install all required dependencies (this will create a `node_modules` folder) and finally run it.
+
+`git clone https://github.com/rdaraujo/react-max-udemy-time-to-practice.git`
+
+`cd react-max-udemy-time-to-practice`
+
+`npm install`
+
+`npm start`
+
+Then open `http://localhost:3000/`.
+
+![Project screenshot](./screenshot-1.png)
+
+![Project screenshot](./screenshot-2.png)
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,5 +1,6 @@
 import Card from "../UI/Card";
 import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
 
 import styles from "./AddUser.module.css";
 
@@ -8,23 +9,24 @@ import { useState } from "react";
 const AddUser = props => {
   const [username, setUsername] = useState("");
   const [age, setAge] = useState("");
+  const [error, setError] = useState();
 
   const addUserHandler = event => {
     event.preventDefault();
 
     if (username.trim() === "") {
-      alert("Invalid username");
+      setError({ title: "Error", message: "Invalid username." });
       return;
     }
     if (age.trim() === "" || Number.parseInt(age) <= 0) {
-      alert("Invalid age");
+      setError({ title: "Error", message: "Invalid age." });
       return;
     }
 
     props.onAddUser({ name: username, age });
-
     setUsername("");
     setAge("");
+    setError();
   };
 
   const usernameInputChangeHandler = event => {
@@ -35,18 +37,31 @@ const AddUser = props => {
     setAge(event.target.value);
   };
 
+  const closeModalHandler = () => {
+    setError();
+  };
+
   return (
-    <Card className={styles.input}>
-      <form onSubmit={addUserHandler}>
-        <label htmlFor="username">Username</label>
-        <input id="username" type="text" value={username} onChange={usernameInputChangeHandler} />
-        <label htmlFor="age">Age (years)</label>
-        <input id="age" type="number" value={age} onChange={ageInputChangeHandler} />
-        <Button type="button" onClick={addUserHandler}>
-          Add User
-        </Button>
-      </form>
-    </Card>
+    <div>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onClose={closeModalHandler}
+        ></ErrorModal>
+      )}
+      <Card className={styles.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          <input id="username" type="text" value={username} onChange={usernameInputChangeHandler} />
+          <label htmlFor="age">Age (years)</label>
+          <input id="age" type="number" value={age} onChange={ageInputChangeHandler} />
+          <Button type="button" onClick={addUserHandler}>
+            Add User
+          </Button>
+        </form>
+      </Card>
+    </div>
   );
 };
 
